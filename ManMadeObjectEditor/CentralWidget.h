cb671da5be6eedace668d684ae162f2d055d21a5
erico <vanini>
2013-10-17 17:32:42 +0200

    zoom

--- ManMadeObjectEditor/CentralWidget.h
+++ ManMadeObjectEditor/CentralWidget.h
@@ -5,10 +5,17 @@
 #include <QWidget>
 #include <QGridLayout>
 #include <QGraphicsScene>
+<<<<<<< HEAD
 #include <QGraphicsView>
 #include "ProfileScene.h"
 #include "FloorScene.h"
 #include "Mesh.h"
+=======
+#include "ProfileScene.h"
+#include "FloorScene.h"
+#include "Mesh.h"
+#include "BasicQGraphicsView.h"
+>>>>>>> Erico
 
 class CentralWidget : public QWidget
 {
@@ -19,9 +26,15 @@ public:
 
     QGridLayout *layout;
     FloorScene *floorScene;
+<<<<<<< HEAD
     QGraphicsView *floorView;
     ProfileScene *profileScene;
     QGraphicsView *profileView;
+=======
+    BasicQGraphicsView *floorView;
+    ProfileScene *profileScene;
+    BasicQGraphicsView *profileView;
+>>>>>>> Erico
 };
 
 #endif // CENTRALWIDGET_H
