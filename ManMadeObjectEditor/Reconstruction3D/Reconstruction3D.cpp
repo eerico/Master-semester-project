03ed0e5f0feab7869bd8794753f080e2b82bae27
Nicolas Maitre <nicolas.maitre@hotmail.com>
2013-11-15 15:28:40 +0100

    15.11.2013

--- ManMadeObjectEditor/Reconstruction3D/Reconstruction3D.cpp
+++ ManMadeObjectEditor/Reconstruction3D/Reconstruction3D.cpp
@@ -593,7 +593,7 @@ void Reconstruction3D::interChainHandling(std::vector< std::vector< Edge* >* >& 
 {
     // le code est faux mais idée est juste. 1 edge se split en 2 et on reconstruit en dessus mais comment update dans
     // la priority queue pour chaque intersection ?
-
+    //
 
 
 
