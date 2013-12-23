8888f6c2a4dbde4c5fb41cb43c279897e2a98d1d
Nicolas Maitre <nicolas.maitre@hotmail.com>
2013-12-23 17:43:43 +0100

    debug

--- ManMadeObjectEditor/Geometry/Edge.cpp
+++ ManMadeObjectEditor/Geometry/Edge.cpp
@@ -267,6 +267,11 @@ std::ostream& operator<<(std::ostream& out, Edge& e) {
 }
 
 Plan* Edge::getDirectionPlan() {
+    if(directionPlan != 0) {
+        if(vertex1 != directionPlan->getVertex()) {
+            std::cerr << "coollll" << std::endl;
+        }
+    }
     return directionPlan;
 }
 
